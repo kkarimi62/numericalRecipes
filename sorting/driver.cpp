f6180603b038acb6199437ae98159ba81987dd91
Kamran <Home@MacBook-Pro-5.local>
2019-09-24 22:32:59 -0600

    selection routine added to sort.h

--- sorting/driver.cpp
+++ sorting/driver.cpp
@@ -6,13 +6,19 @@ using namespace std;
 
 int main()
 {
-	vector<int> v = {7,5,16,10,8,1,3,15,23,9,6,4,-3,0,-1,-2,-10,-9,-7};
+	vector<int> v = {-3,0,-4,5,-10,-11,-2,1,3,2,-1};
 	for( int i = 0; i < v.size(); i++ ) 
 		printf("%d\t",v[i]);
-	shell( v );
+//	shell( v );
 //	piksrt( v );
+//	printf("\n");
+//	for( int i = 0; i < v.size(); i++ ) 
+//		printf("%d\t",v[i]);
+//	vector<int> u = v;
+	int k = select( ( v.size() - 1 ) / 2, v );
 	printf("\n");
 	for( int i = 0; i < v.size(); i++ ) 
 		printf("%d\t",v[i]);
+	printf("\nmedian=%d\n",k);
 	return 0;
 }
--- sorting/sort.h
+++ sorting/sort.h
@@ -27,9 +27,9 @@ void piksrt( vector< T > &arr )
 }
 //---
 template < class T >
-//--- sort an array[0...n-1] into ascending order by shell's method 
 void shell( vector< T > &arr )
 {
+//--- sort an array[0...n-1] into ascending order by shell's method 
 	int k, j, i, n = arr.size();
 	T a;
 	int offset = n; //--- determine the starting increment
@@ -52,6 +52,61 @@ void shell( vector< T > &arr )
 		}
 	}
 	}
-
-
 }
+//-- utility function
+template< class T >
+void SWAP( T &a, T &b )
+{
+	T temp = a;
+	a = b;
+	b = temp; 
+}
+//--
+template< class T >
+T select( const int k, vector< T > &arr )
+{
+//--- given k in [0..n-1] returns an array value from arr[0..n-1] such that arr[0:k-1]<arr[k]<arr[k+1:n-1].
+//--- the input array will be rearranged to have this value in index k.
+	int i, ir, j, l, mid, n = arr.size();
+	T a;
+	l = 0;
+	ir = n - 1;
+	for(;;)
+	{
+		if( ir <= l + 1 ) //--- active partition contains 1 or 2 elements
+		{
+			if( ir == l + 1 && arr[ ir ] < arr[ l ] ) //--- case of 2 elements
+				SWAP( arr[ l ], arr[ ir ] );
+			return arr[ k ]; 
+		}
+		else
+		{	
+			mid = ( l + ir ) >> 1; 		 	  //--- choose median of left, center, and right elements
+			SWAP( arr[ mid ], arr[ l + 1 ] ); //--- as partitioning element a. Also rearrange so that arr[l]<arr[l+1],
+			if( arr[ l ] > arr[ ir ] )        //--- arr[ ir ] > arr[ l + 1 ]
+				SWAP( arr[ l ], arr[ ir ] );
+			if( arr[ l + 1 ] > arr[ ir ] )       
+				SWAP( arr[ l + 1 ], arr[ ir ] );
+			if( arr[ l ] > arr[ l + 1 ] )        
+				SWAP( arr[ l ], arr[ l + 1 ] );
+		i = l + 1; //--- initialize ptrs for partitioning
+		j = ir;
+		a = arr[ l + 1 ];  //--- partitioning element
+		for(;;) //--- beginning of innermost loop
+		{
+			do i++; while( arr[ i ] < a ); //--- scan up to find element > a
+			do j--; while( arr[ j ] > a ); //--- scan down to find element < a
+			if( i > j )
+				break; //--- ptrs crossed
+			SWAP( arr[ i ], arr[ j ] );
+		} //--- end of innermost loop
+		arr[ l + 1 ] = arr[ j ]; //--- insert partitioning element
+		arr[ j ] = a; 
+		if( j >= k )
+			ir = j - 1; //--- keep active the partition that contains the 
+		if( j <= k )    //--- kth element
+			l = i;	
+		}
+	}
+}
+
