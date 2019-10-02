c04522d1de257d91e618631d41bacc1b3564eaec
Kamran <Home@MacBook-Pro-5.local>
2019-10-01 22:41:20 -0600

    quick sort added!

--- sorting/driver.cpp
+++ sorting/driver.cpp
@@ -6,19 +6,26 @@ using namespace std;
 
 int main()
 {
-	vector<int> v = {-3,0,-4,5,-10,-11,-2,1,3,2,-1};
+	vector<int> v = {-4,-3,0,5,-10,-11,-2,1,3,2,-1};
 	for( int i = 0; i < v.size(); i++ ) 
 		printf("%d\t",v[i]);
+	printf("\n");
 //	shell( v );
 //	piksrt( v );
 //	printf("\n");
 //	for( int i = 0; i < v.size(); i++ ) 
 //		printf("%d\t",v[i]);
 //	vector<int> u = v;
-	int k = select( ( v.size() - 1 ) / 2, v );
-	printf("\n");
+//	int k = select( ( v.size() - 1 ) / 2, v );
+//	printf("\n");
+//	hpsort( v );	
+//	for( int i = 0; i < v.size(); i++ ) 
+//		printf("%d\t",v[i]);
+//	printf("\nmedian=%d\n",k);
+	quickSort<int> sort_obj( v );
+	sort_obj.sortSubArrs( 0, v.size() - 1 );	
 	for( int i = 0; i < v.size(); i++ ) 
 		printf("%d\t",v[i]);
-	printf("\nmedian=%d\n",k);
+	printf("\n");
 	return 0;
 }
--- sorting/sort.h
+++ sorting/sort.h
@@ -61,6 +61,56 @@ void SWAP( T &a, T &b )
 	a = b;
 	b = temp; 
 }
+//---
+template< class T >
+struct quickSort {
+//--- sort an array[0...n-1] into ascending order by quick sort method 
+	quickSort( vector< T > &array ):arr( array ) {};
+	vector< T > &arr;
+	void sortSubArrs( int l, int ir )
+	{
+		int mid, i, j;
+		T a;
+		if( ir <= l + 1 ) //--- active partition contains 1 or 2 elements
+		{
+			if( ir == l + 1 && arr[ ir ] < arr[ l ] ) //--- case of 2 elements
+				SWAP( arr[ l ], arr[ ir ] );
+			return; 
+		}
+		else	
+		{	
+			mid = ( l + ir ) >> 1; 		 	  //--- choose median of left, center, and right elements
+			SWAP( arr[ mid ], arr[ l + 1 ] ); //--- as partitioning element a. Also rearrange so that arr[l]<arr[l+1],
+			if( arr[ l ] > arr[ ir ] )        //--- arr[ ir ] > arr[ l + 1 ]
+				SWAP( arr[ l ], arr[ ir ] );
+			if( arr[ l + 1 ] > arr[ ir ] )       
+				SWAP( arr[ l + 1 ], arr[ ir ] );
+			if( arr[ l ] > arr[ l + 1 ] )        
+				SWAP( arr[ l ], arr[ l + 1 ] );
+		i = l + 1; //--- initialize ptrs for partitioning
+		j = ir;
+		a = arr[ l + 1 ];  //--- partitioning element
+		for(;;) //--- beginning of innermost loop
+		{
+			do i++; while( arr[ i ] < a ); //--- scan up to find element > a
+			do j--; while( arr[ j ] > a ); //--- scan down to find element < a
+			if( i > j )
+				break; //--- ptrs crossed
+			SWAP( arr[ i ], arr[ j ] );
+		} //--- end of innermost loop
+		arr[ l + 1 ] = arr[ j ]; //--- insert partitioning element
+		arr[ j ] = a; 
+
+		sortSubArrs( l, j - 1 );
+		sortSubArrs( i, ir );
+//		if( j >= k )
+//			ir = j - 1; //--- keep active the partition that contains the 
+//		if( j <= k )    //--- kth element
+//			l = i;	
+		}
+	
+	}
+};
 //--
 template< class T >
 T select( const int k, vector< T > &arr )
@@ -109,4 +159,104 @@ T select( const int k, vector< T > &arr )
 		}
 	}
 }
+//---
+namespace hpsort_util
+{
+	template< class T >
+	void sift_down( vector< T > &ra, const int l, const int r )
+	{
+	//--- carry out the sift-down on element ra(l) to maintain the heap structure. 
+	//--- l and r determine the left and right range of the sift-down
+		int j, jold;
+		T a;
+		a = ra[ l ];
+		jold = l;
+		j = 2 * l + 1;
+		while( j <= r )
+		{
+			if( j < r && ra[ j ] < ra[ j + 1 ] ) //--- compare to the better underling
+				j++;
+			if( a >= ra[ j ] ) //--- found a's level. Terminate the sift-down.
+				break;
+			ra[ jold ] = ra[ j ]; //--- otherwise, demote a and continue
+			jold = j;
+			j = 2 * j + 1;
+		}
+		ra[ jold ] = a; //--- put a into its slot
+	}
+}
+//---
+template< class T >
+void hpsort( vector< T > &ra )
+{
+	//--- sort an array ra[0...n-1] ( in ascending order) using heapsort.
+	//--- ra is replaced on output by its sorted reaarangement
+	int i, n = ra.size();
+	for( i = n / 2 - 1; i >= 0; i-- )
+	//--- the index i, which here determines the 'left' range of the sift-down, i.e., the element
+	//--- to  be sifted down, is decremented from n /2 - 1 down to 0 during the 'hiring' (heap ceation) phase.
+		hpsort_util::sift_down( ra, i, n - 1);
+	for( i = n - 1; i > 0; i-- )
+	{
+	//--- here the 'right' range of the sift-down is decremented from n - 2 down to 0 during 
+	//--- the 'retirement-and-promotion' (heap selection) phase.
+		SWAP( ra[ 0 ], ra[ i ]); 			   //--- clear a space at the end of the array, and retire
+		hpsort_util::sift_down( ra, 0, i - 1); //--- the top of the heap to it. 
+	}
+}
+/*
+//---
+struct HeapSelect
+{
+	//--- object for tracking the m largest values seen thus far in a stream of values
+	int m, n, srtd;
+	vector<double> heap; 
 
+	//--- constructor: the argument is the number of largest values to track
+	HeapSelect( int mm ): m( mm ), n( 0 ), srtd( 0 ), heap( mm, 1.e99 ) {} 
+
+	void add( double val )
+	{
+	//--- assimilate a new value from the stream
+		int j, k;
+		if( n < m ) //--- heap not yet filled
+		{
+			heap[ n++ ] = val; 
+			if( n == m ) 
+				sort( heap ); //--- create initial heap by overkill!
+		}
+		else
+		{
+			if( val > heap[ 0 ] ) //--- put it on the heap?
+			{
+				heap[ 0 ] = val;
+				for( j = 0;;) //--- sift down
+				{
+					k = ( j << 1 ) + 1;
+					if( k > m - 1 )
+						break;
+					if( k != ( m - 1 ) && heap[ k ] > heap[ k + 1 ] )
+						k++;
+					if( heap[ j ] <= heap[ k ] )
+						break;
+					swap( heap[ k ], heap[ j ]);
+					j = k;
+				}
+			}
+			n++;
+		}
+		srtd = 0; //--- mark heap as "unsorted"
+	}
+
+	double report( int k )
+	{
+	//--- return the kth largest value seen so far. k=0 return the largest, k = 1 the 2nd largest
+	//--- k =m-1 the last position tracked. 
+		int mm = MIN( n, m );
+		if( k > mm - 1 ) throw( "heapselect k too big" );
+		if( k == m - 1 ) return heap[ 0 ]; //--- always free, since top of heap
+		if( !srtd ) { sort( heap ); srtd = 1; } //--- otherwise, need to sort the heap
+		return heap[ mm - 1 - k ];
+	}
+}
+*/
